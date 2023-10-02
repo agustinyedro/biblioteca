baf857f7060a048ac4d73c92a96545f44401e6e9
luzelissalde <luzelissalde@gmail.com>
2023-10-02 17:00:01 -0300

    clases vacias

---
+++ src/biblioteca/AccesoData/Conexion.java
@@ -0,0 +1,39 @@
+
+package biblioteca.AccesoData;
+ 
+    import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    
+    
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB = "biblioteca";
+    private static final  String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
+    
+    private Conexion(){}
+    
+    
+    public static Connection getConexion() {
+        if (connection == null) {
+
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL + DB , USUARIO, PASSWORD);
+
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la BD " + ex.getMessage());
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los Drivers " + ex.getMessage());
+
+            }
+        }
+        return connection;
+    }
+
+}
+    
+
