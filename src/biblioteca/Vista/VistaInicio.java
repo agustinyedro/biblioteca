c7e89ca8bbf38fdab740d62ef87a6ca908851c8d
luzelissalde <luzelissalde@gmail.com>
2023-10-25 17:30:38 -0300

    vista inicio mas completo

--- src/biblioteca/Vista/VistaInicio.java
+++ src/biblioteca/Vista/VistaInicio.java
@@ -55,26 +55,24 @@ Thread hilo;
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         jPanel4 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
         jLfrase = new javax.swing.JLabel();
         jBactualiza = new javax.swing.JButton();
-        jPanel3 = new javax.swing.JPanel();
-        jLhora = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
         jLfecha = new javax.swing.JLabel();
+        jLhora = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
-        jPanel2.setBackground(new java.awt.Color(153, 0, 153));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-        jPanel4.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setForeground(new java.awt.Color(255, 255, 255));
+
+        jPanel4.setBackground(new java.awt.Color(204, 0, 204));
         jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel1.setText("Bienvenidos");
-        jPanel4.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(21, 12, 168, 45));
-
-        jLfrase.setFont(new java.awt.Font("Vivaldi", 0, 24)); // NOI18N
+        jLfrase.setFont(new java.awt.Font("Vivaldi", 0, 48)); // NOI18N
         jLfrase.setForeground(new java.awt.Color(51, 51, 51));
-        jPanel4.add(jLfrase, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 70, 480, 130));
+        jPanel4.add(jLfrase, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 70, 770, 80));
 
         jBactualiza.setBackground(new java.awt.Color(255, 255, 255));
         jBactualiza.setText("ok");
@@ -83,58 +81,72 @@ Thread hilo;
                 jBactualizaActionPerformed(evt);
             }
         });
-        jPanel4.add(jBactualiza, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 90, 30, 28));
+        jPanel4.add(jBactualiza, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 80, 30, 28));
 
-        jPanel3.setBackground(new java.awt.Color(153, 0, 153));
-        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLhora.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        jLhora.setForeground(new java.awt.Color(255, 255, 255));
-        jPanel3.add(jLhora, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 30, 105, 35));
+        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel1.setText("Sistema de Gestion para Biblioteca75. Controle y administre de forma optica y fail el flujo de prestamos y devoluciones de libros");
+        jPanel4.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 190, 660, 120));
 
         jLfecha.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-        jLfecha.setForeground(new java.awt.Color(255, 255, 255));
+        jLfecha.setForeground(new java.awt.Color(51, 51, 51));
+
+        jLhora.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jLhora.setForeground(new java.awt.Color(51, 51, 51));
+
+        jLabel2.setBackground(new java.awt.Color(51, 51, 51));
+        jLabel2.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel2.setText("Bienvenidos");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(157, 157, 157)
+                .addGap(23, 23, 23)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, 665, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jLhora, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(79, 79, 79))
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLfecha, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(207, 207, 207)
-                        .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE))))
+                        .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, 876, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(24, Short.MAX_VALUE))))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(46, 46, 46)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLfecha, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jLfecha, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(35, 35, 35)
-                        .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(16, 16, 16)))
+                .addContainerGap()
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, 248, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(86, 86, 86))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLhora, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLfecha, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, 543, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -145,36 +157,30 @@ Thread hilo;
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 30, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jBactualizaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBactualizaActionPerformed
-        
-        
+
         ArrayList<String> frases = new ArrayList<>();
-    frases.add("La lectura es una conversación con los mejores amigos.");
-    frases.add("Un libro abierto es un cerebro que habla;"
+        frases.add("La lectura es una conversación con los mejores amigos.");
+        frases.add("Un libro abierto es un cerebro que habla;"
             + " cerrado, un amigo que espera; "
-                + "olvidado, un alma que perdona;"
+            + "olvidado, un alma que perdona;"
             + " destruido, un corazón que llora." );
-    frases.add("La creatividad es la inteligencia divirtiéndose." );
-    frases.add("El éxito es la suma de pequeños esfuerzos repetidos día tras día." );
-    frases.add("Un libro es un sueño que tienes en tus manos.");
-    frases.add("Leer es viajar sin moverse.");
-    frases.add("La lectura es a la mente lo que el ejercicio al cuerpo.");
-    
-    
-    
-    Random rand = new Random();
-    int indiceAleatorio = rand.nextInt(frases.size());
-    
-    
-    jLfrase.setText(frases.get(indiceAleatorio));
+        frases.add("La creatividad es la inteligencia divirtiéndose." );
+        frases.add("El éxito es la suma de pequeños esfuerzos repetidos día tras día." );
+        frases.add("Un libro es un sueño que tienes en tus manos.");
+        frases.add("Leer es viajar sin moverse.");
+        frases.add("La lectura es a la mente lo que el ejercicio al cuerpo.");
+
+        Random rand = new Random();
+        int indiceAleatorio = rand.nextInt(frases.size());
+
+        jLfrase.setText(frases.get(indiceAleatorio));
     }//GEN-LAST:event_jBactualizaActionPerformed
 
     
@@ -213,12 +219,12 @@ public void FrasesAleatorias (){
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBactualiza;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLfecha;
     private javax.swing.JLabel jLfrase;
     private javax.swing.JLabel jLhora;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     // End of variables declaration//GEN-END:variables
 }
